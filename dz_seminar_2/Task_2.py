e262fe7469c98ae91471df3bc9d5950bfbcc666c
AleksKolychev <aleksushome@gmail.com>
2024-04-11 10:56:31 +0500

    seminar_2

--- dz_seminar_2/Task_2.py
+++ dz_seminar_2/Task_2.py
@@ -6,5 +6,29 @@
 """
 
 
+from fractions import Fraction
 
+def get_fraction():
+    fraction_str = input("Введите дробь в формате a/b: ")
+    numerator, denominator = map(int, fraction_str.split('/'))
+    return Fraction(numerator, denominator)
+
+
+fraction1 = get_fraction()
+fraction2 = get_fraction()
+
+sum_result = fraction1 + fraction2
+product_result = fraction1 * fraction2
+
+print("Сумма дробей:", sum_result)
+print("Произведение дробей:", product_result)
+
+
+# Проверка модулем fractions
+f1 = Fraction(2, 5)
+print(f1)
+f2 = Fraction(4, 5)
+print(f2)
+print(f1 + f2)
+print(f1 * f2)
 
